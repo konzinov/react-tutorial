35af039d2e2984d2d3aa06174cba1c65e67fad77
Emmanuel KONZI <emmanuel.konzi@custy.com>
2022-02-06 20:21:16 +0100

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App"; // Table tutorial+
-// import App from "./Api"; // Api tutorial
-import './index.css';
-
-ReactDOM.render(<App/>, document.getElementById('root'))
